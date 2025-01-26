78c7f8c440447e3d88f1f2b2bc0be948311ba946
basta <jsembasta@gmail.com>
2025-01-27 00:55:17 +0100

    working black box optim

--- MHD_control_cooling/src/MHD_control_cooling.jl
+++ MHD_control_cooling/src/MHD_control_cooling.jl
@@ -1,11 +1,54 @@
-includet("./openfoam/field_utils.jl") # goddamn julia development, stupid mistake probably
-using .FieldUtils
-
-module MHD_control_cooling
 using Revise
 
+includet("./openfoam/field_utils.jl") # goddamn julia development, stupid mistake probably
+
+using LinearAlgebra, BlackBoxOptim, Dates
+
+module MHD_control_cooling
+using Revise, JSON, LinearAlgebra
+
 include("./openfoam/field_utils.jl")
-using .FieldUtils
+
+function evaluate_criterium(time_path::String, cell_indexes::Vector{Int64})::Float64
+    # read all temperatures
+    T = read_field_scalar(joinpath(time_path, "T"))
+    # extract relevant fields
+    T_slice = T[cell_indexes]
+    # calculate criterium
+    return sum(T_slice)/length(T_slice) 
+end
+
+function run_sim(F::Function)
+    @info "Running simulation"
+    centers_field_path = "../2d-example/0/C"
+    field_template_path = "./data/fieldsTemplate.mustache"
+    field_string = create_force_field_string(F, centers_field_path, field_template_path, true)
+    set_field_at_time("../2d-example", "0", field_string, "F")
+    run_case("../2d-example", "./icoHeatExternalForce")
+end
+
+function get_criterium_in_time(case_path::String)
+    criteria = [];
+    slice_idxs = get_slice_xmin(read_field_vector(joinpath(case_path, "0/C")));
+    for i in 1:1000
+        try
+            time_name = string(i)
+            time_path = joinpath(case_path, time_name)
+            J = evaluate_criterium(time_path, slice_idxs)
+            push!(criteria, evaluate_criterium(time_path, slice_idxs))
+        catch LoadError
+            break
+        end
+    end
+    return criteria
+end
+
+function log_experiment_json(dir, name, criteria, desc)
+    json = JSON.json(Dict("name"=>name, "criteria"=>criteria, "description"=>desc))
+    open(joinpath(dir, "$name.json"), "w") do f
+        write(f, json)
+    end
+end
 
 function main()
     centers_field_path = "../../2d-example/0/C"
@@ -18,3 +61,59 @@ function main()
 end
 
 end # module MHD_control_cooling
+
+function baseline_force(x)
+    return [0.0, 0.0, 0.0]
+end
+
+function rotation_force(x) 
+    println("forcefn: $(typeof(x)) $x\t");
+    return cross(x, [0.5, 0.5, 0.5])*50
+end
+
+function central_force(x)
+    return x .- [0.5, 0.5, 0.5]
+end
+
+function parametric_cells_generator(θ, xCells, yCells, dim=3)::Function
+    @assert xCells * yCells * dim == length(θ)
+    function F(X)::Vector{Float64}
+        xPos = ceil(X[1]*xCells+0.01)
+        yPos = ceil(X[2]*yCells+0.01)
+        xPos = clamp(xPos, 1, xCells)
+        yPos = clamp(yPos, 1, yCells)
+
+
+        idx = Int(dim*((xPos-1)*yCells + (yPos-1)) + 1)
+        return θ[idx:idx+(dim-1)]
+    end
+end
+
+quartal_force = parametric_cells_generator([0.04045899007671505, 0.46618114797333665, -0.0009118347788213965, 0.9513413554885509, -0.5429034604752823, 0.8688778971762849, -0.9053939364779692, 0.8857732272567411, 0.31375231902429757, 0.8100247853948019, 0.1093953582080961, 0.5896836503093306, -0.7568366612862325, 0.28160218122185054, -0.9437382625074121, 0.5371512919099412, -0.31551113665600206, 0.8563497146424621, 0.9721859245051305, 0.754209782250431, 0.8677320742981145, 0.8451275884265578, 0.13708732702575843, 0.6131597814970461, -0.04357261653343791, -0.7211213086112536, -0.4732729848866935, 0.5568502379230311, -0.4908464947540193, -0.7370713733435196, 0.2402778390005072, -0.7353868860035349, 0.6945452160383623, -0.3596922091559357, 0.8924828776906942, -0.21973258257016784, -0.6819051740630332, 0.7090368781262668, 0.00818920092892711, -0.48746312068004777, -0.8707585436420283, -0.11206378977506903, 0.9739015828332115, -0.3600719510441425, 0.976073658012101, 0.46613050746984086, 0.8277529174039324, -0.4029413845152499, -0.5915814895174086, 0.9454767050706295, -0.2997066968307025, -0.09128217812603245, -0.28886350420505824, -0.741355776615034, -0.5947942639447024, 0.8196978787420647, 0.40109735507003275, 0.5974378758638021, 0.834443971236232, 0.7518661739829385, -0.4718585128664097, -0.9336703697748423, -0.5566139051954919, -0.9072687619813506, -0.844042694076521, -0.9130447378324676, -0.9738601577920518, -0.24471801742817204, 0.42779733698549965, -0.4545036095952283, 0.5689830501075652, -0.7382464768616737], 6, 6, 2)
+
+
+function main()
+    MHD_control_cooling.run_sim(x -> vcat(quartal_force(x), [0.0]).*50)
+    criteria = MHD_control_cooling.get_criterium_in_time("../2d-example")
+    MHD_control_cooling.log_experiment_json("./experiments", "quartal", criteria, "quartal")
+end
+
+function blackbox(θ)::Float64
+    quartal_force = parametric_cells_generator(θ, 6, 6, 2)
+    MHD_control_cooling.run_sim(x -> vcat(quartal_force(x), [0.0]).*50)
+    criteria = MHD_control_cooling.get_criterium_in_time("../2d-example")
+    timestamp = Dates.format(Dates.now(), "yyyy-mm-dd_HH-MM-SS")
+    name = "blackbox_$(criteria[end])_$(timestamp)"
+    desc = string(θ)
+    MHD_control_cooling.log_experiment_json("./experiments/blackbox_6x6", name, criteria, desc)
+    return criteria[end]
+end
+
+function optimize()
+    res = bboptimize(blackbox, SearchRange = (-1.0, 1.0), NumDimensions = 6*6*2)
+    println("Optimalization result is")
+    println(res)
+    return res
+end
+
+main()
--- MHD_control_cooling/src/openfoam/field_utils.jl
+++ MHD_control_cooling/src/openfoam/field_utils.jl
@@ -1,14 +1,36 @@
-module FieldUtils
-
 using OteraEngine
 using LinearAlgebra
+using Logging
 
-
-export create_force_field_string, set_field_at_time, run_case
 
 function lineToVec(line)
 	coords = split(strip(line, ['(', ')']))
 	return [parse(Float64, x) for x in coords]
+end
+
+function normalize_cells(cells)
+    # Get min and max for each dimension separately
+    min_coords = minimum.(zip(cells...))  # Get minimum of each dimension
+    max_coords = maximum.(zip(cells...))  # Get maximum of each dimension
+    
+    # Calculate range for each dimension
+    ranges = max_coords .- min_coords
+    
+    # Avoid division by zero by replacing zero ranges with 1
+    # Normalize each cell
+    normalized_cells = [
+        (cell .- min_coords) ./ ranges
+        for cell in cells
+    ]
+    for (i, cell) in enumerate(normalized_cells)
+        for dim in 1:3
+            if isnan(cell[dim])
+                normalized_cells[i][dim] = 0.5
+            end 
+        end
+    end
+    
+    return normalized_cells
 end
 
 function create_force_field_string(F::Function, centers_field_path::String, field_template_path::String, normalize::Bool=false)
@@ -25,14 +47,16 @@ function create_force_field_string(F::Function, centers_field_path::String, fiel
     end
 
     cells = [lineToVec(line) for line in lines[cellStart:cellStart+nCells-1]]
-    min_coords = minimum.(eachrow(cells))
-    max_coords = maximum.(eachrow(cells))
-    min_x, min_y, min_z = min_coords
-    max_x, max_y, max_z = max_coords
+    min_coords = minimum(eachrow(cells))
+    max_coords = maximum(eachrow(cells))
+    min_x, min_y, min_z = min_coords[1]
+    max_x, max_y, max_z = max_coords[1]
 
     if normalize
-        normalized_cells = [(cell .- [min_x, min_y, min_z]) ./ ([max_x - min_x, max_y - min_y, max_z - min_z]) for cell in cells]
-        A = F.(normalized_cells)
+        normalized_cells = normalize_cells(cells)
+
+        A = F.(normalized_cells) 
+
     else 
         A = F.(cells) 
     end
@@ -43,6 +67,55 @@ function create_force_field_string(F::Function, centers_field_path::String, fiel
     render = Base.invokelatest(tmp, init=data)
     return render
 end
+
+function read_field_vector(field_path::String)::Matrix
+    cellsText = read(field_path, String)
+    cellStart = 0
+    nCells = 0
+    lines = split(cellsText, "\n")
+    for i in eachindex(lines)
+        if (lines[i] == "(")
+            cellStart = i+1
+            nCells = parse(Int32, lines[i-1])
+            break
+        end
+    end
+    cells = [lineToVec(line) for line in lines[cellStart:cellStart+nCells-1]]
+
+    cells_matrix = zeros(3,size(cells,1))
+    for (i, cell) in enumerate(cells)
+        cells_matrix[:, i] = cell
+    end
+    return cells_matrix
+end
+
+function read_field_scalar(field_path::String)::Vector{Float64}
+    cellsText = read(field_path, String)
+    cellStart = 0
+    nCells = 0
+    lines = split(cellsText, "\n")
+    for i in eachindex(lines)
+        if (lines[i] == "(")
+            cellStart = i+1
+            nCells = parse(Int32, lines[i-1])
+            break
+        end
+    end
+    cells = [parse(Float32, line) for line in lines[cellStart:cellStart+nCells-1]]
+    cells_matrix = zeros(size(cells,1))
+    for (i, cell) in enumerate(cells)
+        cells_matrix[i] = cell
+    end
+    return cells_matrix
+end
+
+function get_slice_xmin(cells::Matrix)
+    min_coords = minimum.(eachrow(cells))
+    tol = 0.0025
+    is_xmin = map(cell -> cell[1] < min_coords[1]+tol, eachcol(cells))
+    return findall(is_xmin)
+end
+
 
 function set_field_at_time(case_path::String, time::String, field_file_string::String, field_name::String)
     if !isdir(case_path)
@@ -59,15 +132,39 @@ function set_field_at_time(case_path::String, time::String, field_file_string::S
     end
 end
 
-function run_case(case_path::String, solver_name::String)
+function run_case(case_path::String, solver_name::String, verbose = false)
     if !isdir(case_path)
         error("Case path $case_path does not exist.")
     end
 
-    run(setenv(`pwd`, dir=case_path))
-    run(setenv(`openfoam2406-run -c $solver_name`, dir=case_path))
-    
+    if verbose
+        run(setenv(`pwd`, dir=case_path))
+        try
+            run(setenv(`openfoam2406-run -c $solver_name`, dir=case_path))
+        catch e
+            if isa(e, Base.IOError)
+                @info "openfoam2406-run failed, trying the other one (For my desktop pc)"
+                run(setenv(`openfoam2406 -c $solver_name`, dir=case_path))
+            else
+                rethrow(e)
+            end
+        end
+    else
+        open("output.txt", "w") do file
+            redirect_stdout(file) do
+                run(setenv(`pwd`, dir=case_path))
+                try
+                    run(setenv(`openfoam2406-run -c $solver_name`, dir=case_path))
+                catch e
+                    if isa(e, Base.IOError)
+                        @info "openfoam2406-run failed, trying the other one (For my desktop pc)"
+                        run(setenv(`openfoam2406 -c $solver_name`, dir=case_path))
+                    else
+                        rethrow(e)
+                    end
+                end
+            end
+        end
+    end    
 end
 
-
-end
